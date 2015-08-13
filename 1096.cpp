6918cbeea4790d7dc8dcea23f1d4a2b159f65bd8
Wilhelmshaven <nntt19@163.com>
2015-08-13 20:40:00 +0800

    add new problems

--- 1096.cpp
+++ 1096.cpp
@@ -1,65 +1,59 @@
 //#include <stdio.h>
-//#include <stdlib.h>
+//#include <math.h>
 //
-//int cmp(const void *a, const void *b)
+//int main(void)
 //{
-//	return *(long *)a - *(long *)b;
-//}
+//	int n;
+//	scanf("%d", &n);
 //
-//int main()
-//{
-//	long n;
-//	long factor[100];
+//	int pivot = n, count = 0;
+//	int start = n, max = 0, tmp = n;
+//	int m = sqrt((double)n) + 1;
 //
-//	scanf("%ld", &n);
-//
-//	int pivot = 0;
-//	for (int i = 2;; i++)
+//	//直接试除即可
+//	for (int i = 2; i <= m; ++i)
 //	{
+//		if (i > n)break;
+//
 //		if (n%i == 0)
 //		{
-//			factor[pivot] = i;
-//			pivot++;
-//			n = n / i;
-//
-//		}
-//		if (i > n)break;
-//	}
-//	if (n != 0)
-//	{
-//		factor[0] = factor[0] * n;
-//		qsort(factor, pivot, sizeof(long), cmp);
-//	}
-//
-//	int count = 1;
-//	int max = 0;
-//	int len = pivot;
-//	for (int i = 0; i < len; i++)
-//	{
-//		if (factor[i + 1] == factor[i] + 1)
-//		{
-//			count++;
-//		}
-//		else
-//		{
+//			pivot = i;
+//			count = 0;
+//			tmp = n;
+//			while (tmp%pivot == 0)
+//			{
+//				tmp = tmp / pivot;
+//				++pivot;
+//				++count;
+//			}
 //			if (count > max)
 //			{
 //				max = count;
-//				pivot = i - max + 1;
+//				start = i;
 //			}
-//			count = 1;
 //		}
+//
 //	}
 //
-//	printf("%d\n", max);
-//	for (int i = pivot;; i++)
+//	//输出
+//	if (max == 0)printf("1\n%d\n", n);
+//	else
 //	{
-//		printf("%ld", factor[i]);
-//		max--;
-//		if (max > 0)printf("*");
-//		else break;
+//		printf("%d\n", max);
+//		for (int i = 0; i < max - 1; ++i)
+//		{
+//			printf("%d*", start + i);
+//		}
+//		printf("%d", start + max - 1);
 //	}
-//
 //
 //	return 0;
 //}
+//
+////0	答案正确	1	308	10 / 10
+////1	答案正确	1	308	2 / 2
+////2	答案正确	1	180	2 / 2
+////3	答案正确	1	308	2 / 2
+////4	答案正确	1	308	2 / 2
+////5	答案正确	1	256	1 / 1
+////6	答案正确	1	308	1 / 1
